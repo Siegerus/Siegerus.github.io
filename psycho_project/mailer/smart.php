df1c2e7069c336b20170112832bf96c454002b20
Siegerus <161329006+Siegerus@users.noreply.github.com>
2024-12-08 10:58:52 +0300

    Update smart.php

--- psycho_project/mailer/smart.php
+++ psycho_project/mailer/smart.php
@@ -18,13 +18,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers               // гуглим, если другая почта
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ivanoff1siegerus@gmail.com';                 // Наш логин  // Это как бы мыло самого сайта, и всю инфу, которая будет вводиться на нём, будет пересылаться уже на нашу почту, которую указали ниже
-$mail->Password = 'yyfl tjuw exhg mzsi';                           // Наш пароль от ящика // использовал пароль приложений
+$mail->Username = '@gmail.com';                 // Наш логин  // Это как бы мыло самого сайта, и всю инфу, которая будет вводиться на нём, будет пересылаться уже на нашу почту, которую указали ниже
+$mail->Password = '';                           // Наш пароль от ящика // использовал пароль приложений
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to        // тоже гуглим, у каждой почты свой
  
-$mail->setFrom('ivanoff1siegerus@gmail.com', 'psycho_project');   // От кого письмо 
-$mail->addAddress('sobay38651@fresec.com');     // Add a recipient   //Это уже наше мыло, на которое будет приходить письмо
+$mail->setFrom('@gmail.com', 'psycho_project');   // От кого письмо 
+$mail->addAddress('@fresec.com');     // Add a recipient   //Это уже наше мыло, на которое будет приходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
