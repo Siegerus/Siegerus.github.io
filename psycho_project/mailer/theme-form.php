e49f987d492ea04929cd76a924064ef7906f0c52
Siegerus <161329006+Siegerus@users.noreply.github.com>
2024-12-08 10:59:26 +0300

    Update theme-form.php

--- psycho_project/mailer/theme-form.php
+++ psycho_project/mailer/theme-form.php
@@ -16,13 +16,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                     
 $mail->Host = 'smtp.gmail.com';          
 $mail->SMTPAuth = true;                               
-$mail->Username = 'ivanoff1siegerus@gmail.com';                
-$mail->Password = 'yyfl tjuw exhg mzsi';                          
+$mail->Username = '@gmail.com';                
+$mail->Password = '';                          
 $mail->SMTPSecure = 'ssl';                           
 $mail->Port = 465;                                 
  
-$mail->setFrom('ivanoff1siegerus@gmail.com', 'psycho_project');   
-$mail->addAddress('ivanoffsiegerus@gmail.com');      
+$mail->setFrom('@gmail.com', 'psycho_project');   
+$mail->addAddress('@gmail.com');      
 
 $mail->isHTML(true);                                 
 
